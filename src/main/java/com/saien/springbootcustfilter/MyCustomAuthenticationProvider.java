d5e23ac7df045586abf9fe56e777bccd39b907ea
Paiker Abbas <paikerabbas@gmail.com>
2021-11-27 19:43:44 +0300

    spring boot project with spring security custom implementation along with DataSource dynamically

--- src/main/java/com/saien/springbootcustfilter/MyCustomAuthenticationProvider.java
+++ src/main/java/com/saien/springbootcustfilter/MyCustomAuthenticationProvider.java
@@ -1,9 +1,17 @@
 package com.saien.springbootcustfilter;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,9 +19,29 @@ public class MyCustomAuthenticationProvider implements AuthenticationProvider {
 
 	private final String secretKey = "password";
 
+	@Autowired
+	private DriverManagerDataSource dataSource;
+
+	
+	
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String requestKey = authentication.getName();
+		
+		dataSource.setUsername("username");
+		dataSource.setPassword("password");
+
+		JdbcUserDetailsManager userManager = new JdbcUserDetailsManager(dataSource);
+
+		JdbcTemplate jdbcTemplate = userManager.getJdbcTemplate();
+		List<UserRequest> users = jdbcTemplate.query("select * from users", (rs, rowNum) -> {
+			UserRequest user = new UserRequest();
+			user.setUsername(rs.getString("username"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		});
+		users.forEach(System.out::println);
+
 		if (requestKey.equals(secretKey)) {
 			MyCustomAuthentication fulluAuthenticated = new MyCustomAuthentication(null, null, null);
 			return fulluAuthenticated;
--- src/main/java/com/saien/springbootcustfilter/ProjectBeanConfiguration.java
+++ src/main/java/com/saien/springbootcustfilter/ProjectBeanConfiguration.java
@@ -1,16 +1,42 @@
 package com.saien.springbootcustfilter;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.AbstractDriverBasedDataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class ProjectBeanConfiguration extends WebSecurityConfigurerAdapter {
+
+//	@Bean
+//	public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/dev");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("root");
+//        return dataSource;
+//	}
+//	
+	@Bean
+	public JdbcUserDetailsManager jdbcUserDetailsManager() {
+		return new JdbcUserDetailsManager(createDataSource());
+	}
+
+	@Bean
+	public DriverManagerDataSource createDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setUrl("jdbc:mysql://localhost:3306/dev");
+		return ds;
+	}
 
 	@Autowired
 	private MyCustomAuthenticationFilter myCustomAuthenticationFilter;
